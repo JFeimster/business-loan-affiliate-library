ff463a4d4028294a277c7433df4997681f0929db
Jason Feimster <jasonfeimster@gmail.com>
2026-05-03 16:14:50 -0400

    Allow Iconify category icons in Next images

--- next.config.mjs
+++ next.config.mjs
@@ -22,6 +22,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.iconify.design',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   // Optionally, add any other Next.js config below
