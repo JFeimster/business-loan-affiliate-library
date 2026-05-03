ac75e028f31ebd25569e651033bbea19b8a5b288
Jason Feimster <jasonfeimster@gmail.com>
2026-05-03 16:15:01 -0400

    Use Next Image for category cards

--- src/components/category/CategoryCard.tsx
+++ src/components/category/CategoryCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 interface CategoryProps {
@@ -10,10 +11,12 @@ const Category: React.FC<CategoryProps> = ({ imageSrc, altText, categoryName }) 
   return (
     <a className="flex cursor-pointer flex-col">
       <figure className="relative h-[86px] w-[86px] overflow-hidden rounded-full bg-light transition-colors duration-300 hover:bg-light-hover">
-        <img
-          className="absolute inset-0 h-full w-full transform rounded-md object-contain p-4 transition-transform duration-300 ease-in-out hover:scale-110"
+        <Image
+          className="absolute inset-0 transform rounded-md object-contain p-4 transition-transform duration-300 ease-in-out hover:scale-110"
           src={imageSrc}
           alt={altText}
+          fill
+          sizes="86px"
         />
       </figure>
       <h3 className="mt-3 text-center text-base font-semibold text-secondary">{categoryName}</h3>
